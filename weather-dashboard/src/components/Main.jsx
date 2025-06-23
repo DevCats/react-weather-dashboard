26277bb47e236e66ffea890df7221c0b98c626ee
DevCats <devcats@proton.me>
2025-06-23 13:12:12 +0100

    * Called API in Main component to retrieve current weather based on location

--- weather-dashboard/src/components/Main.jsx
+++ weather-dashboard/src/components/Main.jsx
@@ -1,4 +1,19 @@
+import { useState, useEffect } from 'react'
+import { fetchData } from '../api/index.js'
 const Main = () => {
+    const [currentWeather, setCurrentWeather] = useState('');
+
+    useEffect(() => {
+        let client = fetchData('current.json', `${localStorage.getItem('latitude')},${localStorage.getItem('longitude')}`);
+        let data = client.request();
+        data.then((response) => {
+            // console.log(response.data);
+            setCurrentWeather(response.data.current);
+        })
+        .catch((error) => {
+            console.log(error); // !!! TODO: Error feedback for user
+        })
+    }, []);
     
     return (
         <>
