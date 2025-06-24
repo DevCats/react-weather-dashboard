80b125dfbb53386487f680c87dd3540468811646
DevCats <devcats@proton.me>
2025-06-24 10:17:28 +0100

    * Refactor * Destructuring prop objects * Improved presentation for mobile devices (will deal with larger brekpoints later)

--- weather-dashboard/src/components/CurrentWeather.jsx
+++ weather-dashboard/src/components/CurrentWeather.jsx
@@ -1,53 +1,64 @@
-import { WiRaindrops, WiStrongWind, WiHumidity, WiDaySunny, WiFog } from 'weather-icons-react'
+import { WiRaindrops, WiStrongWind, WiCloud, WiHumidity, WiDaySunny, WiFog } from 'weather-icons-react'
 import AdditionalWeatherInfo from "./AdditionalWeatherInfo.jsx"
 
 const CurrentWeather = ({ currentWeather }) => {
+    const { cloud, condition, temp_c, feelslike_c, humidity, precip_mm, uv, vis_km, wind_kph } = currentWeather;
 
     return (
         <>
             <div className="current-weather-wrapper">
                 <div className="current-weather">
-                    {/* <div className="weather-icon"></div> */}
-                    <div className="temp-value">
-                        <div className="real-temp">{Math.round(currentWeather.temp_c)} &#8451;</div>
-                        <div className="feels-temp">feels like {Math.round(currentWeather.feelslike_c)} &#8451;</div>
+                    <div className="weather-icon">
+                        <img src={currentWeather ? currentWeather.condition.icon : null}></img>
                     </div>
-                    <div className="summary">{currentWeather ? currentWeather.condition.text : ''}</div>
+                    <div className="temp-summary-wrapper">
+                        <div className="temp-value">
+                            <div className="real-temp">{Math.round(temp_c)} &#8451;</div>
+                            <div className="feels-temp">feels like {Math.round(feelslike_c)} &#8451;</div>
+                        </div>
+                        <div className="summary">{currentWeather ? condition.text : ''}</div>
+                    </div>
                 </div>
                 <div className="additional-info">
                     <AdditionalWeatherInfo 
-                        _value={ currentWeather.precip_mm }
-                        _units={ 'mm' }
+                        _value={ precip_mm }
+                        _units={ 'mm/h' }
                         _name={ 'Precipitation' }
                         _icon={ <WiRaindrops size={32} color={'#AEECEF'} /> }
                     />
                     <AdditionalWeatherInfo 
-                        _value={ currentWeather.wind_kph }
+                        _value={ wind_kph }
                         _units={ 'km/h' }
                         _name={ 'Wind Speed' }
                         _icon={ <WiStrongWind size={32} color={'#AEECEF'} /> }
                     />
                     <AdditionalWeatherInfo 
-                        _value={ currentWeather.humidity }
+                        _value={ cloud }
+                        _units={ '%' }
+                        _name={ 'Cloud Cover' }
+                        _icon={ <WiCloud size={32} color={'#AEECEF'} /> }
+                    />
+                    <AdditionalWeatherInfo 
+                        _value={ humidity }
                         _units={ '%' }
                         _name={ 'Humidity' }
                         _icon={ <WiHumidity size={32} color={'#AEECEF'} /> }
                     />
                     <AdditionalWeatherInfo 
-                        _value={ currentWeather.uv }
+                        _value={ uv }
                         // _units={ '' }
                         _name={ 'UV Index' }
                         _icon={ <WiDaySunny size={32} color={'#AEECEF'} /> }
                     />
                     <AdditionalWeatherInfo 
-                        _value={ currentWeather.vis_km }
+                        _value={ vis_km }
                         _units={ 'km' }
                         _name={ 'Visibility' }
                         _icon={ <WiFog size={32} color={'#AEECEF'} /> }
                     />
                 </div>
             </div>
-            {console.log(currentWeather)}
+            {/* {console.log(currentWeather)} */}
         </>
     )
 }
